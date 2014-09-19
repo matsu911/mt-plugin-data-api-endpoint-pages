83f7cde5fe5286ea310cbad78b80a4d539bc0bd6
Shigeaki Matsumura <matsu911@gmail.com>
2014-09-19 10:37:43 +0900

    fix indent

--- plugins/DataAPIPages/lib/DataAPIPages/Endpoint/Page.pm
+++ plugins/DataAPIPages/lib/DataAPIPages/Endpoint/Page.pm
@@ -4,14 +4,14 @@ use strict;
 use MT::DataAPI::Endpoint::Common;
 
 sub list {
-    my ( $app, $endpoint ) = @_;
+  my ( $app, $endpoint ) = @_;
 
-    my $res = filtered_list( $app, $endpoint, 'page' ) or return;
+  my $res = filtered_list( $app, $endpoint, 'page' ) or return;
 
-    +{  totalResults => $res->{count} + 0,
-        items =>
-            MT::DataAPI::Resource::Type::ObjectList->new( $res->{objects} ),
-    };
+  +{  totalResults => $res->{count} + 0,
+      items =>
+          MT::DataAPI::Resource::Type::ObjectList->new( $res->{objects} ),
+  };
 }
 
 1;
